889a2b0d338eead4856317ac822337967362fbe9
ZHAO Jing <zj@ahstu.cc>
2017-09-14 21:15:13 +0800

    lec01
    CoreJava第一讲

---
+++ lec01/Expression.java
@@ -0,0 +1,12 @@
+public class Expression {
+	public static void main(String[] args) {
+		int x=3;//variable
+		final int y=4;//constant
+		//3;//literal ×ÖÃæÁ¿
+		System.out.println(3 + 4);
+		System.out.println(3 * 4);
+		System.out.println(3 / 4);
+		System.out.println(3.0 / 4);// 3.0/4.0
+		System.out.println();
+	}
+}
