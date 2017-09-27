06f96e794ac9e3d0f82828eb51ce6a93b750b3d7
zj <zj@ahstu.cc>
2017-09-27 09:12:48 +0800

    Block语句概念

---
+++ lec03/BlockDemo.java
@@ -0,0 +1,21 @@
+/**
+理解块语句
+1. 一个；是一个块语句
+2. 一个if ifelse while for都是一个块语句
+3. 用{}括起来的所有语句构成一个块语句
+*/
+public class IfDemo {
+	public static void main(String[] args) {
+		int x = 3;
+		if (x > 4) {
+			;
+			System.out.println("Bingo");
+			System.out.println("haha");
+			System.out.println("haha");
+			System.out.println("haha");
+		}else{
+			
+		}
+
+	}
+}
