37fd7b50abe5d87b88ab7bb5a9a0b43a0a00b1d4
ZHAO Jing <zj@ahstu.cc>
2017-10-24 20:45:00 +0800

    上传文件至 'lec07'

---
+++ lec07/ArraysSorted.java
@@ -0,0 +1,12 @@
+package week07;
+
+import java.util.Arrays;
+
+public class ArraysSorted {
+	public static void main(String[] args) {
+		int a[]={1,3,5,7,9,2,4,6,8,0};
+		System.out.println(Arrays.toString(a));
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+}
---
+++ lec07/GuessNumber.java
@@ -0,0 +1,22 @@
+package week07;
+
+import java.util.Scanner;
+
+public class GuessNumber {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int key = (int) (Math.random() * 100);// [0,99]
+		for (int i = 0; i < 7; i++) {
+			System.out.println("Please Enter your guessed number~");
+			int user = cin.nextInt();
+			if (user == key) {
+				System.out.println("Congratulations!");
+			} else if (user > key) {
+				System.out.println("Too bigger~");
+			} else {
+				System.out.println("Too smaller~");
+			}
+		}
+		cin.close();
+	}
+}
