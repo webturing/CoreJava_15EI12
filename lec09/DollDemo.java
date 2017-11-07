a1dd2178b51aca8655005f77b9c43bc9a9805ab6
ZHAO Jing <zj@ahstu.cc>
2017-11-07 20:22:17 +0800

    上传文件至 'lec09'

---
+++ lec09/DollDemo.java
@@ -0,0 +1,26 @@
+package week09;
+
+public class DollDemo {
+	public static void main(String[] args) {
+		int N = 10000;
+		int first, second, third, no;
+		first = second = third = no = 0;
+		for (int i = 0; i < N; i++) {
+			double x = Math.random();
+			if (x <= 0.05) {
+				System.out.println("First Prize: Iphone 7 plus~");
+				++first;
+			} else if (x <= 0.05 + 0.10) {
+				System.out.println("Second Prize: Iphone 4S~");
+				++second;
+			} else if (x <= 0.05 + 0.10 + 0.15) {
+				System.out.println("Third Prize: Logitch Mouse~");
+				++third;
+			} else {
+				System.out.println("NO PRIZE: Thank you~");
+				++no;
+			}
+		}
+		System.out.println(String.format("%d %d %d %d\n", first, second, third,	no));
+	}
+}
---
+++ lec09/FermatDemo.java
@@ -0,0 +1,18 @@
+package week09;
+
+import java.math.BigInteger;
+
+public class FermatDemo {
+	static BigInteger f(int n) {
+		BigInteger two = new BigInteger("2");
+
+		return two.pow(1 << n).add(BigInteger.ONE);
+
+	}
+
+	public static void main(String[] args) {		
+		for (int i = 0; i <= 10; i++)
+			System.out.println(f(i).isProbablePrime(100));
+		System.out.println(1 - Math.pow(2, -100));
+	}
+}
