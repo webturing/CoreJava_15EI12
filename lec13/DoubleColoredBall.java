b9f7c72cfacd0980be46fd36cb0ec46446b82cfc
ZHAO Jing <zj@ahstu.cc>
2017-12-05 21:52:39 +0800

    修改了双色球的模拟，避免了对原来数据的依赖

--- lec13/DoubleColoredBall.java
+++ lec13/DoubleColoredBall.java
@@ -2,21 +2,26 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
 
-public class DoubleColoredBall {
+public class DoubleColoredBalls {
 	public static void main(String[] args) {
 		Vector<Integer> balls = new Vector<Integer>();
-		for (int i = 1; i <= 15; i++)
+		for (int i = 1; i <= 35; i++)
 			balls.add(i);
 		System.out.println(balls);
 		Collections.shuffle(balls);
 		System.out.println(balls);
-		List<Integer> firstPrize = balls.subList(0, 7);
+		Vector<Integer> firstPrize = new Vector<Integer>();
+		for (int i = 0; i < 7; i++)
+			firstPrize.add(balls.get(i));
 		System.out.println(firstPrize);
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10000000; i++) {
 			Collections.shuffle(balls);
-			// System.out.println(balls);
-			List<Integer> users = balls.subList(0, 7);
-			System.out.println(users);
+			List<Integer> user = balls.subList(0, 7);
+			// System.out.println(user);
+			if (user.equals(firstPrize)) {
+				System.out.println("5,000,000 RMB!");
+			}
+
 		}
 	}
 }
