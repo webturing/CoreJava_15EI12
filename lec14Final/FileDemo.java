9064c1e51cc665c52f7bc2cbdf164f8e9a64f43b
ZHAO Jing <zj@ahstu.cc>
2017-12-11 21:32:16 +0800

    上传文件至 'lec14Final'

---
+++ lec14Final/FileDemo.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class FileDemo {
+	public static void main(String[] args) {
+		File f = new File("C:/");
+		System.out.println(f.isDirectory());
+		System.out.println(f.length());
+		for (String file : f.list())
+			if (file.endsWith(".mp3"))
+				System.out.println(file);
+	}
+}
